05ad7a26920674ca603bcb343eccd5f6d1183a62
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2018-07-02 19:55:05 +0200

    Added some code to dispaly pressure and translated weather description

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ $(document).ready(function(){
     let $cityName = $('.city-name');
     let $cityTemp = $('.city-temp');
     let $citySky = $('.city-sky');
+    let $citySkyIcon = $('.city-sky-icon');
+    let $cityPressure = $('.city-pressure');
 
     function watherFetch(){
         fetch(`https://api.openweathermap.org/data/2.5/weather?q=${$cityInput.val()},pl&units=metric&appid=21aaf70b5fc4f3bd6de175114b2ec26e`)
@@ -12,7 +14,44 @@ $(document).ready(function(){
             console.log(data)
             $cityName.text(`${data.name}`);
             $cityTemp.text(`${data.main.temp}`);
-            $citySky.text(`${data.weather[0].description}`);
+            $cityPressure.text(`${data.main.pressure}`);
+            console.log(`Ikona: http://openweathermap.org/img/w/${data.weather[0].icon}.png`)
+            // $citySky.text(`${data.weather[0].description}`);
+            switch (data.weather[0].description) {
+                default:
+                    text = 'Brak info';
+                    break;
+                case 'clear sky':
+                    text = 'Czyste niebo';
+                    break;
+                case 'few clouds':
+                    text = 'Lekko zachmurzone';
+                    break;
+                case 'scattered clouds':
+                    text = 'Rozproszone chmury';
+                    break;
+                case 'broken clouds':
+                    text = 'Rozbite chmury';
+                    break;
+                case 'shower rain':
+                    text = 'Obfity deszcz';
+                    break;
+                case 'rain':
+                    text = 'Deszcz';
+                    break;
+                case 'thunderstorm':
+                    text = 'Burza';
+                    break;
+                case 'snow':
+                    text = 'Śnieg';
+                    break;
+                case 'mist':
+                    text = 'Mgła';    
+                    break;            
+            }
+            $citySky.text(text);
+            // $citySkyIcon.text(`<img src="http://openweathermap.org/img/w/${data.weather[0].icon}.png">`);
+            
         })
            
     }
@@ -21,10 +60,8 @@ $(document).ready(function(){
         console.log('test klika')
         console.log($cityInput.val())
     }
-    $checkBtn.on('click', checkWeather);
-    $checkBtn.on('click', watherFetch);
-
-
 
     
+    $checkBtn.on('click', checkWeather);
+    $checkBtn.on('click', watherFetch);    
 });
